8f274e72cb14176025e82831e9b7e0b6f2527b89
JongwonYoun <jeffyoun@naver.com>
2020-05-03 18:43:45 +0900

    글 모델 이름을 Blog를 Post로 변경

--- Project/mainApp/admin.py
+++ Project/mainApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog, Comment
+from .models import Post, Comment
 # Register your models here.
-admin.site.register(Blog)
+admin.site.register(Post)
 admin.site.register(Comment)
