209039eebbdfb23dcb21c0b68a12ff6347f9de77
JongwonYoun <jeffyoun@naver.com>
2020-05-03 18:34:01 +0900

    글, 댓글 모델 추가

--- Project/mainApp/models.py
+++ Project/mainApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+
+    author = models.CharField(max_length = 30)
+
+    pub_date = models.DateTimeField('date published')
+
+    modified_date = models.DateTimeField('date modified')
+
+    view_count = models.IntegerField()
+
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank = True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.text[:50]
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length =30)
+    pub_date = models.DateTimeField('date published', auto_now = True)
